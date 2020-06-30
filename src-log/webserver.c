c80317f5f99065533e97b1ed94d46df206100dcc
alancastro90 <alancastro90@gmail.com>
2020-06-30 08:32:48 -0300

    __asm problem

--- src-log/webserver.c
+++ src-log/webserver.c
@@ -318,7 +318,9 @@ struct Request *parse_request(const char *raw) {
 
 uint64_t rdtsc(){
     unsigned int lo,hi;
+    /* clang-format off */
     __asm__ __volatile__ ("rdtsc" : "=a" (lo), "=d" (hi));
+    /* clang-format on */
     return ((uint64_t)hi << 32) | lo;
 }
 
